02f3b8eff53415ea1b11aae7dc1f3fb9457356a0
RedDev <89706070+RedSlyice@users.noreply.github.com>
2022-04-07 21:26:36 +0200

    Add files via upload

---
+++ credit/Credits.java
@@ -0,0 +1,18 @@
+package me.reddev.gui.credit;
+
+
+public enum Credits {
+
+    RedDev("Main Developer"),
+    Cleverjoe09("Main Developer"),
+    Hero("ClickGUI"),
+    EricGolde("Addons and Events"),
+    superblaubeere27("NotificationSystem");
+
+    public String s;
+
+    Credits(String s) {
+        this.s = s;
+    }
+
+}
